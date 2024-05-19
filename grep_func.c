73d47cdbb8c77809dd9877c117e3ff16895ced6f
thenPie <118397975+thenPie@users.noreply.github.com>
2024-05-19 20:22:15 +0300

    import from gitlab

--- grep_func.c
+++ grep_func.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+// #include <cstring>
 
 // Тебе необходимо разработать утилиту grep:
 // - Поддержка следующих флагов: `-e`, `-i`, `-v`, `-c`, `-l`, `-n`
@@ -28,9 +29,19 @@
 // `grep [options] template [file_name]`
 
 void run_grep(int argc, char** argv) {
+
+    // debug
+    printf("Function: run_grep\n");
+
     struct Flags flags = {-1, -1, -1, -1, -1, -1};
     int count_files;
-    char** existing_files = list_of_existing_files(argc, argv, &count_files);;
+    int count_patterns;
+    char** existing_files = list_of_existing_files(argc, argv, &count_files);
+    char** patterns = list_of_patterns(argc, argv, &count_patterns, existing_files, count_files);
+
+    // debug
+    printf("Count patterns: %d\n", count_patterns);
+
     int valid = 0;
     
     valid = check_if_there_are_any_arguments(argc);
@@ -39,30 +50,108 @@ void run_grep(int argc, char** argv) {
         opter(&flags, argc, argv);
 
         // delete later
-        print_existing_file_names(existing_files, count_files);
+        // print_existing_file_names(existing_files, count_files);
+        // print_patterns(patterns, count_patterns);
+        print_existing_file_names(patterns, count_patterns);
     }
 
-    // freeing file names
-    if (existing_files != NULL) {
-        for (int i = 0; i < count_files; i++) {
-            if (existing_files[i] != NULL) {
-                free(existing_files[i]);
-            }
-        }
-        free(existing_files);
-    }
+    free_list(patterns, count_patterns);
+    free_list(existing_files, count_files);
 
     // delete this line later
-    printf("%d is the final valid variable", valid);
+    if (valid == -1) {
+        printf("Program didn't start");
+    } else {
+        printf("Program worked");
+    }
 }
 
-void print_existing_file_names(char** file_list, int count_files) {
-    for (int i = 0; i < count_files; i++) {
-        printf("%s\n", file_list[i]);
+void free_list(char** list, int count) {
+    if (list != NULL) {
+        for (int i = 0; i < count; i++) {
+            if (list[i] != NULL) {
+                free(list[i]);
+            }
+        }
+        free(list);
     }
+}
+
+// TODO: make list_functions into different functions, code duplication!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+char** list_of_patterns(int argc, char** argv, int* count_patterns, char** file_list, int count_files) {
+
+    // debug
+    printf("Function: list_of_patterns\n");
+
+    char** pattern_list = (char**)malloc((argc - 1) * sizeof(char*));
+    if (pattern_list == NULL) {
+        perror("Cannot see patterns");
+        exit(EXIT_FAILURE);
+    } else {
+        *count_patterns = 0;
+        char exclude_list[MAX_ARRAY_SIZE][MAX_SINGLE_ARRAY_ENTRY_SYMBOLS] = {
+            "-e",
+            "-i",
+            "-v",
+            "-c",
+            "-l",
+            "-n"
+        };
+        int exclude_list_index = 6;
+
+        for (int i = 0; i < count_files; i++) {
+            strncpy(exclude_list[exclude_list_index], file_list[i], MAX_SINGLE_ARRAY_ENTRY_SYMBOLS - 1);
+            exclude_list[exclude_list_index][MAX_SINGLE_ARRAY_ENTRY_SYMBOLS - 1] = '\0';
+            exclude_list_index += 1;
+        }
+
+        // debug, shows exclusion list
+        // for (int i = 0; i < exclude_list_index; i++) {
+        //     printf("%s\n", exclude_list[i]);
+        // }
+
+        printf("ENTERING INTO LOOP\n");
+        // elements in exclude list shall be ignored, anything else is used as a template for searching in a file
+        for (int i = 1; i < argc; i++) {
+            int res = is_in_exclude_list(argv[i], exclude_list, exclude_list_index);
+            printf("%d\n", res);
+            if (res == 0) {
+                pattern_list[*count_patterns] = strdup(argv[i]);
+                (*count_patterns)++;
+            }
+        }
+        printf("DONE WITH THE LOOP\n");
+        
+        pattern_list = (char**)realloc(pattern_list, (*count_patterns) * sizeof(char*));
+        if (pattern_list == NULL && *count_patterns > 0) {
+            perror("Couldn't readjust memory to amount of patterns");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return pattern_list;
+}
+
+int is_in_exclude_list(char* string, char exclude_list[MAX_SINGLE_ARRAY_ENTRY_SYMBOLS][MAX_ARRAY_SIZE], int exclude_list_index) {
+    
+    printf("Function: is_in_exclude_list\n");
+
+    int result = 0;
+    for (int i = 0; i < exclude_list_index; i++) {
+        int check_res = strcmp(string, exclude_list[i]);
+        if (check_res == 0) {
+            result = -1;
+        }
+    }
+    printf("Returning %d\n", result);
+    return result;
 }
 
 char** list_of_existing_files(int argc, char** argv, int* count_files) {
+
+    // debug
+    printf("Function: list_of_existing_files\n");
+
     char** file_list = (char**)malloc((argc - 1) * sizeof(char*));
     if (file_list == NULL) {
         perror("Cannot see files");
@@ -91,7 +180,29 @@ char** list_of_existing_files(int argc, char** argv, int* count_files) {
     return file_list;
 }
 
+void print_existing_file_names(char** file_list, int count_files) {
+
+    printf("Function: print_existing_file_names\n");
+
+    for (int i = 0; i < count_files; i++) {
+        printf("%s\n", file_list[i]);
+    }
+}
+
+void print_patterns(char** pattern_list, int count_patterns) {
+
+    printf("Function: print_patterns\n");
+
+    for (int i = 0; i < count_patterns; i++) {
+        printf("%s\n", pattern_list[i]);
+    }
+}
+
 void opter(struct Flags* flags, int argc, char** argv) {
+
+    // debug
+    printf("Function: opter\n");
+
     int opt;
     while ((opt = getopt(argc, argv, ":eivcln")) != -1) {
         switch (opt) {
@@ -120,12 +231,19 @@ void opter(struct Flags* flags, int argc, char** argv) {
 }
 
 void print_all_argvs(int argc, char** argv) {
+
+    printf("Function: print_all_argvs\n");
+
     for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
 }
 
 void print_flags(struct Flags flags) {
+
+    // debug
+    printf("Function: print_flags\n");
+
     printf("e is - %d\n", flags.e);
     printf("i is - %d\n", flags.i);
     printf("v is - %d\n", flags.v);
@@ -135,6 +253,10 @@ void print_flags(struct Flags flags) {
 }
 
 int check_if_there_are_any_arguments(int argc) {
+
+    // debug
+    printf("Function: check_if_there_are_any_arguments\n");
+
     int result = -1;
     if (argc > 1) {
         result = 0;
--- grep_func.h
+++ grep_func.h
@@ -1,6 +1,9 @@
 #ifndef GREP_FUNC_H
 #define GREP_FUNC_H
 
+#define MAX_ARRAY_SIZE 1000
+#define MAX_SINGLE_ARRAY_ENTRY_SYMBOLS 1000
+
 struct Flags {
     int e;
     int i;
@@ -14,9 +17,14 @@ void run_grep(int argc, char** argv);
 
 int check_if_there_are_any_arguments(int argc);
 char** list_of_existing_files(int argc, char** argv, int* count_files);
+char** list_of_patterns(int argc, char** argv, int* count_patterns, char** file_list, int count_files);
+int is_in_exclude_list(char* string, char exclude_list[MAX_SINGLE_ARRAY_ENTRY_SYMBOLS][MAX_ARRAY_SIZE], int exclude_list_index);
 void print_existing_file_names(char** file_list, int count_files);
+void print_patterns(char** pattern_list, int count_patterns);
 void opter(struct Flags* flags, int argc, char** argv);
 void print_all_argvs(int argc, char** argv);
 void print_flags(struct Flags flags);
+
+void free_list(char** list, int count);
 
 #endif
